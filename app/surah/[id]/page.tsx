da855d9e5242362f739fd5854aa9c1edec17c97a
Ekbal41 <mdasifekbalshagor@gmail.com>
2026-01-16 20:12:21 +0600

    up

--- app/surah/[id]/page.tsx
+++ app/surah/[id]/page.tsx
@@ -4,18 +4,23 @@ interface PageProps {
   params: { id: string };
 }
 
+export async function generateStaticParams() {
+  return Array.from({ length: 114 }, (_, i) => ({
+    id: String(i + 1),
+  }));
+}
+
 export default async function SurahPage({ params }: PageProps) {
   const { id } = await params;
 
-  const res1 = await fetch(`https://quranapi.pages.dev/api/${id}.json`, {
-    cache: "force-cache",
-  });
-  const api1Data: any = await res1.json();
+  const [res1, res2] = await Promise.all([
+    fetch(`https://quranapi.pages.dev/api/${id}.json`, {
+      cache: "force-cache",
+    }),
+    fetch(`https://api.quran.gading.dev/surah/${id}`, { cache: "force-cache" }),
+  ]);
 
-  const res2 = await fetch(`https://api.quran.gading.dev/surah/${id}`, {
-    cache: "force-cache",
-  });
-  const api2Data: any = await res2.json();
+  const [api1Data, api2Data] = await Promise.all([res1.json(), res2.json()]);
 
   const surahData = {
     ...api1Data,
